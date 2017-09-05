448fe402a76b0f208b6deb1e76244af9926cd3b8
teckel12 <tim@leethost.com>
2017-09-05 19:49:29 -0400

    Added heading pointer and altitude bar

--- iNav.lua
+++ iNav.lua
@@ -6,7 +6,8 @@
 -- Use at your own risk!
 
 local flightMode = ""
-local holdMode = ""
+local armedPrev = false
+local headingRef = 0
 
 local function init()
   -- init is called once when model is loaded
@@ -54,23 +55,26 @@ local function run(event)
 
   -- *** Satellites ***
   sats = tonumber(string.sub(getValue("Tmp2"), -2))
-  lcd.drawText(95, 9, "Sats " .. sats, SMLSIZE)
+  lcd.drawText(90, 9, "Sats " .. sats, SMLSIZE)
 
   -- *** GPS Coords ***
   gpsLatLon = getValue("GPS")
   if (type(gpsLatLon) == "table") then
-    --lcd.drawChannel(95, 17, "GAlt", SMLSIZE)
-    lcd.drawText(96, 17, string.format("%5d", getValue("GAlt")) .. "ft", SMLSIZE)
-    lcd.drawText(82, 25, displayLat(gpsLatLon["lat"]), SMLSIZE)
-    lcd.drawText(82, 33, displayLon(gpsLatLon["lon"]), SMLSIZE)
+    lcd.drawText(91, 17, string.format("%5d", getValue("GAlt")) .. "ft", SMLSIZE)
+    lcd.drawText(77, 25, displayLat(gpsLatLon["lat"]), SMLSIZE)
+    lcd.drawText(77, 33, displayLon(gpsLatLon["lon"]), SMLSIZE)
   else
-    lcd.drawFilledRectangle(88, 17, 40, 23, INVERS)
-    lcd.drawText(93, 20, "No GPS", INVERS)
-    lcd.drawText(101, 30, "Fix", INVERS)
+    lcd.drawFilledRectangle(83, 17, 40, 23, INVERS)
+    lcd.drawText(88, 20, "No GPS", INVERS)
+    lcd.drawText(96, 30, "Fix", INVERS)
   end
 
   -- *** Decode flight mode ***
   mode = getValue("Tmp1")
+  holdMode = ""
+  headFree = false
+  headingHold = false
+  altHold = false
   showHold = true
   extra = 0
   armed = false
@@ -117,8 +121,7 @@ local function run(event)
     end
     if (modeB >= 4) then
       modeB = modeB - 4
-      holdMode = "HF"
-      extra = BLINK + INVERS
+      headFree = true
     end
     if (modeC >= 4) then
       modeC = modeC - 4
@@ -129,14 +132,13 @@ local function run(event)
     end
     if (modeC >= 2) then
       modeC = modeC - 2
+      altHold = true
       if (posHold) then
-        holdMode = "3D"
-      else
-        holdMode = "AH"
+        flightMode = "3DHOLD"
       end
     end
     if (modeC == 1) then
-      holdMode = holdMode .. "HH"
+      headingHold = true
     end  
     if (modeB >= 2) then
       modeB = modeB - 2
@@ -162,34 +164,44 @@ local function run(event)
 
   -- *** Direction ***
   if (armed) then
-    lcd.drawRectangle(46, 8, 42, 25, INVERS)
+    if (armedPrev == false) then
+      headingRef = getValue("Hdg")
+    end
+    --lcd.drawRectangle(55, 8, 24, 24)
+    local heading = getValue("Hdg") - headingRef
+    local rad1 = math.rad(heading)
+    local rad2 = math.rad(heading + 155)
+    local rad3 = math.rad(heading - 155)
+    local x1 = math.sin(rad1) * 11 + 67
+    local y1 = 20 - (math.cos(rad1) * 11)
+    local x2 = math.ceil(math.sin(rad2) * 11) + 67
+    local y2 = 20 - math.ceil(math.cos(rad2) * 11)
+    local x3 = math.sin(rad3) * 11 + 67
+    local y3 = 20 - (math.cos(rad3) * 11)
+    lcd.drawLine(x1, y1, x2, y2, SOLID, FORCE)
+    lcd.drawLine(x1, y1, x3, y3, SOLID, FORCE)
+    if (headingHold) then
+      lcd.drawFilledRectangle((x2 + x3) / 2 - 2, (y2 + y3) / 2 - 2, 5, 5, SOLID)
+    else
+      lcd.drawLine(x2, y2, x3, y3, DOTTED, FORCE)
+    end
   else
-    lcd.drawChannel(50, 17, "Hdg", SMLSIZE)
+    lcd.drawChannel(52, 17, "Hdg", SMLSIZE)
   end
+  if (headFree) then
+    lcd.drawText(48, 9, "HEADFREE", SMLSIZE + INVERS + BLINK)
+  end
+  armedPrev = armed
 
-  -- *** Display flight mode ***
+  -- *** Display flight mode (centered) ***
   displayMode = flightMode
-  if (showHold and holdMode ~= "") then
-    displayMode = flightMode .. " " .. holdMode
-  end
-  pos = 48
-  if (string.len(displayMode) < 4) then
-    pos = 67
-  elseif (string.len(displayMode) < 5) then
-    pos = 62
-  elseif (string.len(displayMode) < 6) then
-    pos = 57
-  elseif (string.len(displayMode) < 7) then
-    pos = 52
-  end
+  lcd.drawText(47, 33, displayMode, SMLSIZE + extra)
+  pos = 47 + (87 - lcd.getLastPos()) / 2
+  lcd.drawFilledRectangle(46, 32, 40, 10, ERASE)
   lcd.drawText(pos, 33, displayMode, SMLSIZE + extra)
 
   -- *** Data ***
-  lcd.drawText(0, 9, "Altd", SMLSIZE)
-  lcd.drawText(0, 17, "Dist", SMLSIZE)
-  lcd.drawText(0, 25, "Sped", SMLSIZE)
-  lcd.drawText(0, 33, "Curr", SMLSIZE)
-  if (armed == true) then
+  if (armed) then
     altitude = getValue("Alt")
     distance = getValue("Dist")
     speed = getValue("GSpd")
@@ -200,24 +212,36 @@ local function run(event)
     distance = getValue("Dist+")
     speed = getValue("GSpd+")
     current = getValue("Curr+")
+    lcd.drawFilledRectangle(0, 8, 20, 32, INVERS)
     tags = SMLSIZE + INVERS
-    lcd.drawFilledRectangle(20, 8, 26, 32, INVERS)
   end
-  lcd.drawText(21, 9, math.floor(altitude), tags)
+  if (altHold and showHold) then
+    lcd.drawText(0, 9, "Altd ", SMLSIZE + INVERS)
+  else
+    lcd.drawText(0, 9, "Altd", tags)
+  end
+  lcd.drawText(0, 17, "Dist", tags)
+  lcd.drawText(0, 25, "Sped", tags)
+  lcd.drawText(0, 33, "Curr", tags)
+  altitudeTags = SMLSIZE
+  if (altHold and showHold) then
+    altitudeTags = SMLSIZE + INVERS
+  end
+  lcd.drawText(22, 9, math.floor(altitude), altitudeTags)
   if (altitude < 1000) then
-    lcd.drawText(lcd.getLastPos(), 9, "ft", tags)
+    lcd.drawText(lcd.getLastPos(), 9, "ft", altitudeTags)
   end
-  lcd.drawText(21, 17, math.floor(distance * 3.28084), tags)
+  lcd.drawText(22, 17, math.floor(distance * 3.28084), SMLSIZE)
   if (distance < 1000) then
-    lcd.drawText(lcd.getLastPos(), 17, "ft", tags)
+    lcd.drawText(lcd.getLastPos(), 17, "ft", SMLSIZE)
   end
-  lcd.drawText(21, 25, math.floor(speed), tags)
+  lcd.drawText(22, 25, math.floor(speed), SMLSIZE)
   if (speed < 100) then
-    lcd.drawText(lcd.getLastPos(), 25, "mph", tags)
+    lcd.drawText(lcd.getLastPos(), 25, "mph", SMLSIZE)
   end
-  lcd.drawNumber(21, 33, current, tags + PREC1)
+  lcd.drawNumber(22, 33, current, SMLSIZE + PREC1)
   if (current < 100) then
-    lcd.drawText(lcd.getLastPos(), 33, "A", tags)
+    lcd.drawText(lcd.getLastPos(), 33, "A", SMLSIZE)
   end
 
   -- *** Bar graphs ***
@@ -228,22 +252,26 @@ local function run(event)
     cell = batt / cells
   end
   lcd.drawText(0, 42, "Batt", SMLSIZE)
-  lcd.drawNumber(21, 42, batt, SMLSIZE + PREC1)
+  lcd.drawNumber(22, 42, batt, SMLSIZE + PREC1)
   lcd.drawText(lcd.getLastPos(), 42, "V", SMLSIZE)
-  lcd.drawGauge(46, 41, 82, 7, math.min(math.max(cell - 3.3, 0) * 111.1, 98), 100)
+  lcd.drawGauge(46, 41, 77, 7, math.min(math.max(cell - 3.3, 0) * 111.1, 98), 100)
 
   fuel = getValue("Fuel")
   lcd.drawText(0, 50, "Fuel", SMLSIZE)
-  lcd.drawText(21, 50, fuel .. "%", SMLSIZE)
-  lcd.drawGauge(46, 49, 82, 7, math.min(fuel, 98), 100)
+  lcd.drawText(22, 50, fuel .. "%", SMLSIZE)
+  lcd.drawGauge(46, 49, 77, 7, math.min(fuel, 98), 100)
 
   rssi = getValue("RSSI")
   lcd.drawText(0, 58, "RSSI", SMLSIZE)
-  lcd.drawText(21, 58, rssi .. "dB", SMLSIZE)
-  lcd.drawGauge(46, 57, 82, 7, math.min(rssi, 98), 100)
-  min = 79 * (getValue("RSSI-") / 98) + 47
+  lcd.drawText(22, 58, rssi .. "dB", SMLSIZE)
+  lcd.drawGauge(46, 57, 77, 7, math.min(rssi, 98), 100)
+  min = 74 * (math.min(getValue("RSSI-"), 98) / 98) + 47
   lcd.drawLine(min, 58, min, 62, SOLID, ERASE)
 
+  -- Altitude
+  lcd.drawRectangle(124, 9, 4, 55, SOLID)
+  height = math.max(math.min(math.ceil(getValue("Alt") / 400 * 53), 53), 1)
+  lcd.drawRectangle(125, 63 - height, 2, height, SOLID)
   return 1
 end
 
